ffaf3bf52621ed15a28117338595827b7bc3a732
Blink Chen <blink@fluxuscompute.com>
2023-12-06 15:57:05 +0800

    update: test ci

--- src/openai/mod.rs
+++ src/openai/mod.rs
@@ -11,3 +11,18 @@ struct ChatRequest {
 }
 
 pub async fn chat() {}
+
+pub async fn get_available_key(db: &PgPool) -> Result<String, sqlx::Error> {
+    let key = nanoid!(16);
+    let result = sqlx::query!(
+        r#"select count(*) as count from openai_key where key = $1"#,
+        key
+    )
+    .fetch_one(db)
+    .await?;
+    if result.count == 0 {
+        Ok(key)
+    } else {
+        get_available_key(db).await
+    }
+}
